f0d41ddf3a6ebb8aad22a3612b2ec62dfa5dd199
Maya <maya.hubbard@outlook.com>
2024-01-04 17:02:21 -0800

    generator is working. added media query.

--- index.js
+++ index.js
@@ -4,8 +4,34 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 const passOne = document.getElementById("pw-one");
 const passTwo = document.getElementById("pw-two");
 
+const generate = document.getElementById("gen-btn");
+
 passOne.textContent = "blue";
 passTwo.textContent = "green";
 
 // function to generate random passwords
 // needs to generate two unique passwords at once
+
+function randomPassword() {
+    let password = "";
+    //loop through the characters array
+    for(let i = 0; i < 15; i++) {
+        let input = Math.floor(Math.random() * characters.length);
+        password += characters[input];
+    }
+    return password;
+}
+
+// function makePassword() {
+    
+// }
+
+//button click
+generate.addEventListener("click", function() {
+    const newPassOne = randomPassword();
+    const newPassTwo = randomPassword();
+
+    passOne.textContent = newPassOne;
+    passTwo.textContent = newPassTwo;
+});
+
